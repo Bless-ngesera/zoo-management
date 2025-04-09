562889e004ace3006d0115fe43026383303fe276
Bless_ngesera <ngeserabless@gmail.com>
2025-04-09 18:23:33 +0300

    Update database.php file
    update Database configuration,
    update Initialize PDO,
    fix error handling

--- config/database.php
+++ config/database.php
@@ -1,13 +1,14 @@
 <?php
-$host = 'localhost';
-$dbname = 'zoo_management';
-$username = 'root';
-$password = 'mysql061004!';
+// Database configuration
+define('DSN', 'mysql:host=localhost;dbname=zoo_management;charset=utf8mb4');
+define('DB_USER', 'root');
+define('DB_PASS', 'mysql061004!');
 
+// Initialize PDO
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO(DSN, DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die('Error: Could not connect to the database. ' . $e->getMessage());
 }
 ?>
