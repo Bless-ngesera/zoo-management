09c8c70ec217488113883bc21b747f136030a8dd
Bless_ngesera <ngeserabless@gmail.com>
2025-04-09 18:22:36 +0300

    Update constants.php file
    fix the project path

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,5 @@
 <?php
-define('BASE_URL', 'http://localhost/zoo-management/');
+define('BASE_URL', '/zoo-management/');
 define('USER_URL', BASE_URL . 'user/');
 define('ADMIN_URL', BASE_URL . 'admin/');
 define('ASSETS_URL', BASE_URL . 'assets/');
